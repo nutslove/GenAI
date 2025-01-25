54b23b8314e2e13424d8c367f470a8642bae0ec6
Lee Joonki <nuts_love86@yahoo.co.jp>
2025-01-26 01:56:09 +0900

    modified

--- SlackBot/SocketMode/app.py
+++ SlackBot/SocketMode/app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
@@ -6,12 +7,28 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.event("app_mention")
-def message_hello(event, say):
+def message_hello(event, say, ack, client):
+    ack()
+
     # イベントがトリガーされたチャンネルへ say() でメッセージを送信
-    text = event["text"]
-    print(text)
-    # say(f"メンションを受け取りました: {text}")
-    say(f"こんにちは、<@{event['user']}> さん！")
+    input_text = re.sub("<@.+>", "", event["text"]).strip() # botへのメンションを削除
+    thread_ts = event.get("thread_ts", event["ts"])  # スレッドタイムスタンプを取得
+
+    # メンションされたメッセージにリアクションを追加
+    client.reactions_add(
+        channel=event["channel"],
+        name="thumbsup",  # 追加するスタンプの名前（例: "thumbsup"）
+        timestamp=event["ts"]  # メンションされたメッセージのタイムスタンプ
+    )
+
+    say(
+        text=f"こんにちは、<@{event['user']}> さん！",
+        thread_ts=thread_ts
+    )
+    say(
+        text=f"次のメッセージを受け取りました: {input_text}",
+        thread_ts=thread_ts
+    )
     
 
 if __name__ == "__main__":
